1e0b7cafd1a6582f152f22bc670d32df7864f527
Gayathri575 <gayathriii575@gmail.com>
2025-10-27 17:09:38 +0530

    Reverse

--- src/paractise.java
+++ src/paractise.java
@@ -1,9 +1,18 @@
 public class paractise {
-    public static void main(String [] args){
-        int sum = 0;
-        int arr [] = {1,3,4,5,78,90,45,3,2,1};
-        for(int i = 0 ; i < arr.length ; i++)
-        sum += arr[i] ;
-        System.out.println(sum);
+    public static void main (String [] args){
+        int arr [] = {12,23,45,23,24,90,5};
+        int Start = 0 ;
+        int end = arr.length - 1;
+        while ( Start < end){
+            int temp = arr[Start];
+            arr[Start] = arr[end];
+            arr[end] = temp ;
+
+            Start ++;
+            end --;
+        }
+        for(int i = 0 ; i <arr.length ; i++)
+        System.out.print(arr[i] + " ");
     }
+
 }
