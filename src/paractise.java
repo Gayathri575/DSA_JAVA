0825acfd7d7d635e6e75219dca68ed96045456fe
Gayathri575 <gayathriii575@gmail.com>
2025-10-27 20:58:36 +0530

    Second Largest Number

--- src/paractise.java
+++ src/paractise.java
@@ -1,18 +1,23 @@
 public class paractise {
-    public static void main (String [] args){
-        int arr [] = {12,23,45,23,24,90,5};
-        int Start = 0 ;
-        int end = arr.length - 1;
-        while ( Start < end){
-            int temp = arr[Start];
-            arr[Start] = arr[end];
-            arr[end] = temp ;
+    public static void main(String[] args) {
+        int arr[] = {12, 23, 45, 24, 90, 5};
 
-            Start ++;
-            end --;
+        // sort ascending order
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] > arr[j]) {  // swap for ascending
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
         }
-        for(int i = 0 ; i <arr.length ; i++)
-        System.out.print(arr[i] + " ");
-    }
 
+        System.out.print("Ascending Order: ");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+
+        System.out.println("\nSecond Largest: " + arr[arr.length - 2]);
+    }
 }
