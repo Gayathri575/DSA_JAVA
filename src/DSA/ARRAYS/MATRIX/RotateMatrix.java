5d0bc60467169c2b0ac52e920cf9fa32ca601053
Gayathri575 <gayathriii575@gmail.com>
2025-10-30 20:30:00 +0530

    Rotate Matrix into 90 degree clockwise

---
+++ src/DSA/ARRAYS/MATRIX/RotateMatrix.java
@@ -0,0 +1,38 @@
+package DSA.ARRAYS.MATRIX;
+
+public class RotateMatrix {
+    public static void main(String[] args) {
+        int[][] arr = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
+
+        int n = arr.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int temp = arr[i][j];
+                arr[i][j] = arr[j][i];
+                arr[j][i] = temp;
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            int start = 0, end = n - 1;
+            while (start < end) {
+                int temp = arr[i][start];
+                arr[i][start] = arr[i][end];
+                arr[i][end] = temp;
+                start++;
+                end--;
+            }
+        }
+
+        System.out.println("Rotated Matrix (90° Clockwise):");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
