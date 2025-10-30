80ac98d6f027e192024354494535ae6b8730b5ac
Gayathri575 <gayathriii575@gmail.com>
2025-10-30 15:50:40 +0530

    Array Basics ( sum , min , max , reverse , second largest , even and odd )

--- src/DSA/ARRAYS/Simple.java
+++ src/DSA/ARRAYS/Simple.java
@@ -1,37 +1,68 @@
 package DSA.ARRAYS;
 
 public class Simple {
-    public static void main(String [] args){
-        int arr [] = {1,2,3,4,5,6,7,8};
-        int Sum = 0;
-        int Start = 0;
-        int End = arr.length - 1 ;
-        int Min = arr[0];
-        int Max = arr[0];
+    public static void main(String[] args) {
 
+        int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 10};
+        int sum = 0;
+        int start = 0;
+        int end = arr.length - 1;
+        int min = arr[0];
+        int max = arr[0];
 
-        while ( Start < End){
-            int temp = arr[Start];
-            arr[Start] = arr[End];
-            arr[End] = temp ;
-
-            Start++;
-            End--;
+        // Reverse the array
+        while (start < end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
         }
-        System.out.println("The Reversed Array is :");
-        for(int i = 0 ; i < arr.length ; i++){
-            Sum += arr[i];
-            if(arr[i] > Max){
-                Max = arr[i];
-            }
-            if(arr[i] < Min){
-                Min = arr[i];
-            }
-            System.out.print(arr[i]+ " ");
+
+        System.out.println("The Reversed Array is:");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
         }
-        System.out.println("\nthe Minimum is :" + Min);
-        System.out.println("the Maximum is :" + Max);
-        System.out.println("the Sum is :" + Sum);
+
+        //  Even numbers
+        System.out.println("\n\nThe Even Numbers are:");
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % 2 == 0) {
+                System.out.print(arr[i] + " ");
+            }
+        }
+
+        //  Odd numbers
+        System.out.println("\nThe Odd Numbers are:");
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % 2 != 0) {
+                System.out.print(arr[i] + " ");
+            }
+        }
+
+        //  Find sum, min, max, and second largest
+        int first = Integer.MIN_VALUE;
+        int second = Integer.MIN_VALUE;
+
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+
+            if (arr[i] > max) max = arr[i];
+            if (arr[i] < min) min = arr[i];
+
+            // Second largest logic
+            if (arr[i] > first) {
+                second = first;
+                first = arr[i];
+            } else if (arr[i] > second && arr[i] != first) {
+                second = arr[i];
+            }
+        }
+
+        //  Print results
+        System.out.println("\n\nThe Minimum is: " + min);
+        System.out.println("The Maximum is: " + max);
+        System.out.println("The Sum is: " + sum);
+        System.out.println("The Second Largest is: " + second);
     }
-
 }
