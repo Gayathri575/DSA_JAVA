6c536e5ac7bd5cb56f077b2d864a2e108f489828
Gayathri575 <gayathriii575@gmail.com>
2025-10-31 22:02:55 +0530

    Reverse String

--- src/DSAStrings/CountVowelsConsonants.java
+++ src/DSAStrings/CountVowelsConsonants.java
@@ -9,7 +9,7 @@ public class CountVowelsConsonants {
         for (int i = 0; i < str.length(); i++) {
         char ch = str.charAt(i);
 
-        if (ch >= 'a' && ch <= 'z') { // check only alphabets
+        if (ch >= 'a' && ch <= 'z') {
             if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
                 vowels++;
             } else {
