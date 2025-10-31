f96d795eb446ae6957c60f26c16edce3bed252d9
Gayathri575 <gayathriii575@gmail.com>
2025-10-31 22:12:38 +0530

    Change Case

---
+++ src/DSAStrings/ChangeCase.java
@@ -0,0 +1,24 @@
+package DSAStrings;
+
+public class ChangeCase {
+    public static void main(String[] args) {
+        String str = "TrishaLove";
+        String result = "";
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+
+            if (Character.isLowerCase(ch)) {
+                result += Character.toUpperCase(ch);
+            } else if (Character.isUpperCase(ch)) {
+                result += Character.toLowerCase(ch);
+            } else {
+                result += ch; // for symbols/spaces
+            }
+        }
+
+        System.out.println("Original String: " + str);
+        System.out.println("Converted String: " + result);
+    }
+
+}
