e5f35413d16b55f40430cb8b08dfc81822d5d20d
Gayathri575 <gayathriii575@gmail.com>
2025-10-31 22:08:59 +0530

    Palindrome Check

---
+++ src/DSAStrings/PalindromeCheck.java
@@ -0,0 +1,18 @@
+package DSAStrings;
+
+public class PalindromeCheck {
+    public static void main(String[] args) {
+        String str = "madam";
+        String reversed = "";
+
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reversed += str.charAt(i);
+        }
+
+        if (str.equals(reversed)) {
+            System.out.println(str + " is a Palindrome ");
+        } else {
+            System.out.println(str + " is NOT a Palindrome ");
+        }
+    }
+}
