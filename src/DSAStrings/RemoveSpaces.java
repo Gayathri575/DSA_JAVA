87d35e053ecd4ab1fa2863dccbf3ea1271faa6b3
Gayathri575 <gayathriii575@gmail.com>
2025-11-01 22:21:58 +0530

    Remove space

---
+++ src/DSAStrings/RemoveSpaces.java
@@ -0,0 +1,9 @@
+package DSAStrings;
+
+public class RemoveSpaces {
+    public static void main(String[] args) {
+        String str = "Removing spaced from each word";
+        String result = str.replace(" ", "");
+        System.out.println("After removing spaces: " + result);
+    }
+}
